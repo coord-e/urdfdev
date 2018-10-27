2429194e1f21a28ed5e67bcd276c1e387e21f776
coord.e <me@coord-e.com>
2018-10-27 12:35:06 +0900

    Add: Reload rviz when changes are detected

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+
 model_path=$1
 urdf_path=$2
+pid_path=$3
 
 if [[ "$model_path" = *.xacro ]]; then
-  /ros_entrypoint.sh rosrun xacro xacro --xacro-ns "$model_path" > $urdf_path
+  rosrun xacro xacro --xacro-ns "$model_path" > $urdf_path
 else
   cp "$model_path" "$urdf_path"
 fi
+
+if [ -s "$pid_path" ]; then
+  kill $(cat $pid_path)
+fi
+
+rosparam set robot_description -t "$urdf_path"
+rosrun rviz rviz -d $(rospack find urdf_tutorial)/rviz/urdf.rviz &
+echo "$!" > $pid_path
+echo "Built $urdf_path and restarted rviz $(cat $pid_path)"
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,19 @@
 
 model_path=$1
 urdf_path=$(mktemp)
+pid_path=$(mktemp)
 
-build_cmd="/build.sh $model_path $urdf_path"
+build_cmd="/build.sh $model_path $urdf_path $pid_path"
 
-eval $build_cmd
 
 export DISPLAY=:0
 Xvfb $DISPLAY -screen 0 1024x768x24 +extension GLX +render -noreset &
 x11vnc -display $DISPLAY -rfbport 5900 &
 /tmp/noVNC/utils/launch.sh --vnc localhost:5900 &
-/ros_entrypoint.sh roslaunch urdf_tutorial display.launch model:=$urdf_path &
+
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+roscore &
+rosparam set use_gui true
+
+eval $build_cmd
 fswatch --event Created --event Updated --event Removed --event Renamed --recursive $(pwd) | xargs -n1 $build_cmd
