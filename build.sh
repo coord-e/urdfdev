a71c6ef00357fc6947af1672e978cc8a69d12993
coord.e <me@coord-e.com>
2018-10-27 15:03:21 +0900

    Fix: Kill *_state_publishers via ps&grep
    Because `rosnode kill <nonexistent node>` causes infinity loop without
    tty

--- build.sh
+++ build.sh
@@ -11,8 +11,7 @@ else
   cp "$model_path" "$urdf_path"
 fi
 
-rosnode list | grep rviz | xargs rosnode kill
-rosnode kill joint_state_publisher robot_state_publisher
+rosnode list | grep -e rviz -e joint_state_publisher -e robot_state_publisher | xargs rosnode kill
 # Kill all joint_state_publisher processes, which is left after `rosnode kill`
 ps a | grep "[j]oint_state_publisher" | awk '{print $1}' | xargs kill -9
 
