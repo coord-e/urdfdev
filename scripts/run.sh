3a7bef150751d622badb9d9e151ed82606b8718f
coord.e <me@coord-e.com>
2018-10-27 20:42:28 +0900

    Change: Log to $URDFENV_LOG and print then only in verbose mode

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,16 +11,19 @@ display_size=${URDFENV_DISPLAY_SIZE:-1024x768}
 novnc_port=${URDFENV_NOVNC_PORT:-6080}
 
 export DISPLAY=:0
-Xvfb $DISPLAY -screen 0 ${display_size}x24 +extension GLX +render -noreset &
-fluxbox &
-x11vnc -display $DISPLAY -rfbport 5900 -noxrecord -xkb -bg
-/opt/urdfenv/noVNC/utils/launch.sh --vnc localhost:5900 --listen ${novnc_port} &
+Xvfb $DISPLAY -screen 0 ${display_size}x24 +extension GLX +render -noreset &>> $URDFENV_LOG &
+fluxbox -log $URDFENV_LOG &> /dev/null &
+x11vnc -display $DISPLAY -rfbport 5900 -noxrecord -xkb -bg -o $URDFENV_LOG
+/opt/urdfenv/noVNC/utils/launch.sh --vnc localhost:5900 --listen ${novnc_port} &>> $URDFENV_LOG &
 
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-roscore &
-wait-for-it ${ROS_MASTER_URI#*//}
+roscore &>> $URDFENV_LOG &
+wait-for-it ${ROS_MASTER_URI#*//} &>> $URDFENV_LOG
 
-rosparam set use_gui true
+rosparam set use_gui true &>> $URDFENV_LOG
 
 eval $build_cmd false
+
+wait-for-it localhost:$novnc_port && echo "Ready. go to http://localhost:6800"
+
 fswatch --event Created --event Updated --event Removed --event Renamed --recursive $URDFENV_FSWATCH_ADDITIONAL_OPTIONS $sources | xargs -n1 $build_cmd true
--- urdfenv_entrypoint.sh
+++ urdfenv_entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+export URDFENV_LOG=${URDFENV_LOG:-/var/log/urdfenv.log}
 /opt/urdfenv/run.sh $@ &
 pid=$!
 trap "kill $pid" 1 2 3 15
+
+if [ "${URDFENV_VERBOSE:-}" != "" ]; then
+  tail -f $URDFENV_LOG &
+fi
 
 wait $pid
