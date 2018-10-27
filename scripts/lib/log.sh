47ba419213593cf3ce8450a00e7c3bf291e2586f
coord.e <me@coord-e.com>
2018-10-27 22:04:18 +0900

    Add: Add exec_log_ and log what is executed

--- scripts/lib/log.sh
+++ scripts/lib/log.sh
@@ -2,6 +2,11 @@ function info() {
   tput setaf 2 && echo -n "=> " && tput sgr0 && tput bold && echo $@ && tput sgr0
 }
 
+function exec_log_() {
+  echo "+ $@" >> $URDFDEV_LOG
+  $@
+}
+
 function exec_log() {
-  $@ &>> $URDFDEV_LOG
+  exec_log_ $@ &>> $URDFDEV_LOG
 }
