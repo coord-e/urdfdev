f5469c51ead72c9f7c5c9eae687abbf46002247a
coord.e <me@coord-e.com>
2018-10-27 17:55:44 +0900

    Change: Make vnc port configurable via URDFENV_NOVNC_PORT

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,13 @@ urdf_path=$(mktemp)
 build_cmd="/build.sh $model_path $urdf_path"
 
 display_size=${URDFENV_DISPLAY_SIZE:-1024x768}
+novnc_port=${URDFENV_NOVNC_PORT:-6080}
 
 export DISPLAY=:0
 Xvfb $DISPLAY -screen 0 ${display_size}x24 +extension GLX +render -noreset &
 fluxbox &
 x11vnc -display $DISPLAY -rfbport 5900 -noxrecord -xkb -bg
-/tmp/noVNC/utils/launch.sh --vnc localhost:5900 &
+/tmp/noVNC/utils/launch.sh --vnc localhost:5900 --listen ${novnc_port} &
 
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 roscore &
