4e5920e333fe4219e0edb36fcb491c4738e14d44
coord.e <me@coord-e.com>
2018-10-31 20:37:13 +0900

    Fix: Fix the layout of messages

--- scripts/lib/log.sh
+++ scripts/lib/log.sh
@@ -1,5 +1,5 @@
 function info() {
-  tput setaf 2 && echo -n " >   " && tput sgr0 && echo "$@" && tput sgr0
+  tput setaf 2 && echo -n "   > " && tput sgr0 && echo "$@" && tput sgr0
 }
 
 function error() {
@@ -7,11 +7,11 @@ function error() {
 }
 
 function status() {
-  tput setaf 2 && echo -n " =>  " && tput sgr0 && tput bold && echo "$@" && tput sgr0
+  tput setaf 2 && echo -n "  => " && tput sgr0 && tput bold && echo "$@" && tput sgr0
 }
 
 function cmd_info() {
-  tput setaf 2 && echo -n "  +  " && tput sgr0 && echo "$@" && tput sgr0
+  tput setaf 2 && echo -n "   + " && tput sgr0 && echo "$@" && tput sgr0
 }
 
 function exec_log_() {
